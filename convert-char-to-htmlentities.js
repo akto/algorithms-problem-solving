f1fde31f3deb74e5b80383c347a47ba6fba2c73d
Egemen <yusege@gmail.com>
2017-12-08 10:19:17 +0300

    create convert-char-to-htmlentities.js

---
+++ convert-char-to-htmlentities.js
@@ -0,0 +1,26 @@
+function convertHTML(str) {
+  var re = /[&<>"']/g;
+  return str.replace(re,replacer);
+}
+
+function replacer( match, r1, r2, r3, r4, r5, offset, string ){
+    console.log('Matched substring: '+match+' - '+ characters[match]);
+    return characters[match];
+}
+
+var characters = {
+    "&" : "&amp;",
+    "<" : "&lt;",
+    ">" : "&gt;",
+    "\"" : "&quot;",
+    "\'" : "&apos;"
+}
+
+console.clear();
+convertHTML("Dolce & Gabbana");
+convertHTML("Hamburgers < Pizza < Tacos");
+convertHTML("Sixty > twelve");
+convertHTML('Stuff in "quotation marks"');
+convertHTML("Shindler's List");
+convertHTML("<>");
+convertHTML("abc");
