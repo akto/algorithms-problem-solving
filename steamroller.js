6991d1d1e035603c33780c77a68f97e815a4ad79
Egemen <yusege@gmail.com>
2018-01-20 22:46:32 +0300

    final solution for this algorithm

--- steamroller.js
+++ steamroller.js
@@ -1,24 +1,24 @@
-var newArray = newArray.length > 0 ? [] : newArray;
-function steamrollArray(arr) {
-  // I'm a steamroller, baby
-  recursive( arr );
-}
+var newArray = [];
 
-function recursive(arr){
-  arr.map( function( i ){
-    if( Array.isArray( i ) ){ steamrollArray(i); }
-    else{ newArray.push(i); }
-  });
+function steamrollArray(arr) {
+// Mostly newArray has latest array from recursion
+// so I need to reset it
+  if( newArray.length > 0 ){ newArray = []; }
+  recursive( arr );
+// newArray shouldn't return anything in recursive func
   console.log(newArray);
   return newArray;
 }
-
+function recursive( arr ){
+  arr.map( function( i,e ){
+      if( Array.isArray( i ) ){ recursive(i); }
+      else{ newArray.push(i); }
+  });
+}
 console.clear();
 console.time();
-
 steamrollArray([[["a"]], [["b"]]]); //should return ["a", "b"].
 steamrollArray([1, [2], [3, [[4]]]]); //should return [1, 2, 3, 4].
 steamrollArray([1, [], [3, [[4]]]]); //should return [1, 3, 4].
 steamrollArray([1, {}, [3, [[4]]]]); //should return [1, {}, 3, 4]
-//newArray = [];
 console.timeEnd();
