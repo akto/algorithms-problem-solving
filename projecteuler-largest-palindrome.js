a3c165c71f11ba5d6b1b61ed784bb03fccdd835e
Egemen <yusege@gmail.com>
2018-02-06 22:17:48 +0300

    create projecteuler-largest-palindrom.js

---
+++ projecteuler-largest-palindrome.js
@@ -0,0 +1,29 @@
+/*
+largestPalindrome( firstNumber, SecondNumber, initialValueFor1stNumber, initialValueFor2ndNumber)
+to check the largest palindrome number between firstNumber and secondNumber 
+need to specify initial values for both numbers
+if these initial values is close to number then it takes less execution time
+*/
+function largestPalindrome( b,c,k,l ){
+    var i,j,
+        largest = 0;
+    for( i = k; i <= b; i++ ){
+        for( j = l; j <= c; j++){
+            var bc = i*j,
+                bstring = bc.toString();
+            if( isPalindrome( bstring ) ){ 
+              largest = largest > bc ? largest : bc;              
+            }
+        }
+    }
+    return largest;
+}
+
+function isPalindrome(str){
+  return str === str.split('').reverse().join('');
+}
+
+console.clear();
+console.time();
+console.log( largestPalindrome( 999, 999, 900, 900 ) );
+console.timeEnd();
