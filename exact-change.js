a37a198c92f3dc095f114e80dfc9123bcb9fa969
Egemen <yusege@gmail.com>
2018-02-07 22:19:32 +0300

    update

--- exact-change.js
+++ exact-change.js
@@ -1,7 +1,20 @@
 function checkCashRegister(price, cash, cid) {
   var change;
   // Here is your change, ma'am.
+  console.log( Money.penny );
   return change;
+}
+
+var Money = {
+	penny : 0.01,
+	nickel : 0.05,
+	dime : 0.1,
+	quarter : 0.25,
+	one : 1,
+	five : 5,
+	ten : 10,
+	twenty : 20,
+	hundred : 100
 }
 
 // Example cash-in-drawer array:
