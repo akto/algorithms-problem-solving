90c271f2fc19ca7868585f2e0d7b5f99cd965b87
Egemen <yusege@gmail.com>
2018-12-06 10:01:36 +0300

    edit

--- factorial.js
+++ factorial.js
@@ -10,4 +10,7 @@ function factorialize(num) {
 
 factorialize(5);
 
-const factorial = n => n == 1 ? n : n*fact(n-1);
+const factorial = n => n == 1 ? n : n*factorial(n-1);
+
+factorial(5);
+//-> 120
